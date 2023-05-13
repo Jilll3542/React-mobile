ae1bd5857b3c3dae8cac7e35f3376e6ce17b0fea
Jilll3542 <xu20213542@163.com>
2023-05-13 14:56:12 +0800

    项目搭建- redux环境配置

--- geek-h5/src/index.js
+++ geek-h5/src/index.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import store from "@/store"
+import { Provider } from 'react-redux'
 //引入通用样式
 
 import "./assets/styles/index.scss"
 
-ReactDOM.render(<App/>,document.getElementById('root'))
+ReactDOM.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>
+    ,document.getElementById('root'))
