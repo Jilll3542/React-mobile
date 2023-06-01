c5c6e387ed7a4a8c26e85f34bbdc69786950a5fe
Jilll3542 <xu20213542@163.com>
2023-06-01 16:48:59 +0800

    个人中心完成

---
+++ geek-h5/src/store/actions/profile.js
@@ -0,0 +1,55 @@
+import request from "@/utils/request";
+import { SAVE_PROFILE, SAVE_USER } from "../action_types/profile";
+
+/**
+ * 保存用户信息
+ * @param {*} payload
+ * @returns
+ */
+export const saveUser = (payload) => {
+  return {
+    type: SAVE_USER,
+    payload,
+  };
+};
+/**
+ * 获取用户信息
+ * @returns Promise
+ */
+export const getUser = () => {
+  return async (dispatch) => {
+    const res = await request.get("/user");
+    dispatch(saveUser(res.data));
+  };
+};
+
+export const saveProfile = (payload) => {
+  return {
+    type: SAVE_PROFILE,
+    payload,
+  };
+};
+
+export const getProfile = () => {
+  return async (dispatch) => {
+    const res = await request.get("/user/profile");
+    console.log(res.data);
+    dispatch(saveProfile(res.data));
+  };
+};
+
+// 修改用户的信息
+export const updateProfile = (data) => {
+  return async (dispatch) => {
+    await request.patch("/user/profile", data);
+    dispatch(getProfile());
+  };
+};
+
+//上传用户头像信息
+export const updatePhoto = (fd) => {
+  return async (dispatch) => {
+    await request.patch("/user/photo", fd);
+    dispatch(getProfile());
+  };
+};
--- geek-h5/src/store/reducers/login.js
+++ geek-h5/src/store/reducers/login.js
@@ -1,12 +1,14 @@
-
 const initValue = {
-    token:" ",
-    refresh_token:" ",
-}
-export default function reducer(state = initValue,action){
-    const {type,payload} = action
-    if(type==="login/token"){
-        return payload
-    }
-    return state
+  token: " ",
+  refresh_token: " ",
+};
+export default function reducer(state = initValue, action) {
+  const { type, payload } = action;
+  if (type === "login/token") {
+    return payload;
+  }
+  if (type === "login/logout") {
+    return {};
+  }
+  return state;
 }
