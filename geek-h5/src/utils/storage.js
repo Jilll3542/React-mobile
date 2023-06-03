9d96bb60adb4f90d3957db2fffbbb00f6e3c2066
Jilll3542 <xu20213542@163.com>
2023-06-03 20:43:44 +0800

    频道管理

--- geek-h5/src/utils/storage.js
+++ geek-h5/src/utils/storage.js
@@ -1,5 +1,6 @@
 // 用户 Token 的本地缓存键名
 const TOKEN_KEY = "geek-itcast-21";
+const CHANNEL_KEY = "geek-itcast-21-channel";
 /** * 从本地缓存中获取 Token 信息 */
 export const getTokenInfo = () => {
   return JSON.parse(localStorage.getItem(TOKEN_KEY)) || {};
@@ -15,4 +16,16 @@ export const removeTokenInfo = () => {
 /** * 判断本地缓存中是否存在 Token 信息 */
 export const hasToken = () => {
   return !!getTokenInfo().token;
+};
+/**保存频道数据到本地 */
+export const setLocalChannels = (channels) => {
+  localStorage.setItem(CHANNEL_KEY, JSON.stringify(channels));
+};
+/**获取本地的频道数据，没有默认数据不要默认为空数组 */
+export const getLocalChannels = () => {
+  return JSON.parse(localStorage.getItem(CHANNEL_KEY));
+};
+/**刪除本地的频道数据 */
+export const removeLocalChannels = () => {
+  localStorage.removeItem(CHANNEL_KEY);
 };
